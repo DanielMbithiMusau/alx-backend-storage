85b14a8350527c0ca83c414e325b3f2369fb2ec8
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-08-11 14:04:47 +0300

    Uniq Users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create Users table
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE
 );
